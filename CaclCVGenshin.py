4f63288e5b276e9157388c600fb02335277c8f68
Admin <ngoquangthang2003@gmail.com>
2023-02-08 08:55:46 +0700

    Add rating table

--- CaclCVGenshin.py
+++ CaclCVGenshin.py
@@ -1,3 +1,6 @@
+import os
+
+
 def rateCV(cv):
     if (cv < 10):
         print("No upgrade")
@@ -17,12 +20,24 @@ def caclCV():
     crit_damage = float(input("Enter your crit damage: "))
 
     cv = crit_damage + (crit_rate) * 2
-    print(cv)
+    print("Your CV: ", cv)
     rateCV(cv)
 
+def ratingTable():
+    print("-----RATING TABLE-----")
+    print("CV < 10: No upgrade")
+    print("CV 10-20: Average")
+    print("CV 20-30: Decent")
+    print("CV 30-40: Very Good")
+    print("CV 40-50: Jewel")
+    print("CV > 50: is this thing still exists??")
+    print("----------------------")
+
 while(True):
+    ratingTable()
     caclCV()
     choice = input("Do you want to caculate another artifact?(Y/N): ").capitalize()
     if choice!="Y":
         print("Bye")
+        os.system("pause")
         break;
